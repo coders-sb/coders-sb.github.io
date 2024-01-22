c4adb13dea2ac13f572bb31a9fbcadee0202c22e
rohildshah <shah5963@gmail.com>
2024-01-22 10:55:24 -0800

    About page sizing and layout

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,19 +1,6 @@
 import Navbar from "../components/navbar";
 import officers from "../../utils/officers";
 
-function officerCard(officer) {
-	return (
-		<div className="flex flex-col justify-center items-center w-1/3 p-4">
-			<img src={officer["image"]} className="w-1/3 rounded-full" />
-			<h1 className="text-2xl font-bold text-center">{officer["name"]}</h1>
-			<h2 className="text-xl font-bold text-center">{officer["position"]}</h2>
-			<h3 className="text-lg font-bold text-center">{officer["major"]}</h3>
-			<h4 className="text-lg font-bold text-center">{officer["year"]}</h4>
-			<h5 className="text-lg font-bold text-center">{officer["email"]}</h5>
-		</div>
-	);
-}
-
 function section(title, id) {
 	return (
 		<>
@@ -26,12 +13,12 @@ function section(title, id) {
 					officers[id].map((officer, index) => {
 						return (
 							<div className="flex flex-col justify-center items-center w-1/3 p-4">
+								<img src={officer["image"] || "public/officers/empty.jpg"} className="w-3/4 p-2" />
 								<h1 className="text-lg font-bold text-center">{officer["name"]}</h1>
-								<img src={officer["image"] || "public/officers/empty.jpg"} className="w-1/2 p-2" />
 								<hr className="w-1/2" />
 								<h2 className="text-base text-center">{officer["role"]}</h2>
 								<hr className="w-1/2 pt-4" />
-								{/* <h3 className="text-base text-center px-8">{officer["description"]}</h3> */}
+								<h3 className="text-base text-center">{officer["description"]}</h3>
 							</div>
 						)
 					})
@@ -45,12 +32,13 @@ function About() {
 	return (
 		<>
 			<Navbar />
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center mx-20 px-10">
 				<h1 className="text-4xl font-bold text-center">About</h1>
-				<img src="public/officers/about.jpg" className="w-3/4 p-4" />
+				<img src="public/officers/about.jpg" className="w-full p-10" />
 			
-				<div className="flex flex-col items-start mb-5">
-					<h2 className="text-xl">A team of like-minded students ready to help you learn the skills to succeed</h2>
+				<div className="flex flex-col items-start mb-5 w-4/5">
+					<h2 className="text-xl mb-2">A team of like-minded students ready to help you learn the skills to succeed!</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id aliquam aliquam, nunc nunc aliquet nunc, vitae aliquam nunc nunc vitae. Sed euismod, diam id aliquam aliquam, nunc nunc aliquet nunc, vitae aliquam nunc nunc vitae.</p>
 				</div>
 				<div className="flex flex-col items-center">
 					{ section("Executive Board", "executive") }
